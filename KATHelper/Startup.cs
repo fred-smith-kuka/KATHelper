848b750f677671895ae7674b57e292d20de95924
Fred Smith <52112076+fred-smith-kuka@users.noreply.github.com>
2019-06-23 22:22:20 -0400

    new

--- KATHelper/Startup.cs
+++ KATHelper/Startup.cs
@@ -47,6 +47,15 @@ namespace KATHelper
 
             // Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
             services.AddTransient<IBot, DialogAndWelcomeBot<MainDialog>>();
+            
+            // Add the HttpClientFactory to be used for the QnAMaker calls.
+            services.AddHttpClient();
+
+            // Create the Bot Framework Adapter with error handling enabled.
+            services.AddSingleton<IBotFrameworkHttpAdapter, AdapterWithErrorHandler>();
+
+            // Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
+            services.AddTransient<IBot, QnABot>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
